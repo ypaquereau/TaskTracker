83fb618ba9f5c02b4682ec5970ccd444c3aca586
Yoann Paquereau <yoann.paquereau@gmail.com>
2022-11-05 22:49:25 +0100

    add fix for Doctrine PostgreSQL Default Schema

---
+++ src/EventListener/FixPostgreSQLDefaultSchemaListener.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\PostgreSQLSchemaManager;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+
+// From https://gist.github.com/vudaltsov/ec01012d3fe27c9eed59aa7fd9089cf7
+final class FixPostgreSQLDefaultSchemaListener
+{
+    /**
+     * @throws SchemaException
+     * @throws Exception
+     */
+    public function postGenerateSchema(GenerateSchemaEventArgs $args): void
+    {
+        $schemaManager = $args
+            ->getEntityManager()
+            ->getConnection()
+            ->createSchemaManager()
+        ;
+
+        if (!$schemaManager instanceof PostgreSqlSchemaManager) {
+            return;
+        }
+
+        $schema = $args->getSchema();
+
+        foreach ($schemaManager->getExistingSchemaSearchPaths() as $namespace) {
+            if (!$schema->hasNamespace($namespace)) {
+                $schema->createNamespace($namespace);
+            }
+        }
+    }
+}
