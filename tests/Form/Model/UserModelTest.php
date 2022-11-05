adf9f9423889319acb676aa934fffaec3307b1f1
Yoann Paquereau <yoann.paquereau@gmail.com>
2022-11-05 23:42:42 +0100

    add test for unique user email

--- tests/Form/Model/UserModelTest.php
+++ tests/Form/Model/UserModelTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Tests\Form\Model;
 
+use App\DataFixtures\UserFixtures;
 use App\Form\Model\UserModel;
 use Exception;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -43,6 +46,29 @@ final class UserModelTest extends KernelTestCase
         $mailError = $errors->get(0);
         $this->assertEquals('email', $mailError->getPropertyPath());
         $this->assertEquals('This value is not a valid email address.', $mailError->getMessage());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUniqueEmail(): void
+    {
+        /**
+         * @var AbstractDatabaseTool $databaseToolCollection
+         * @phpstan-ignore-next-line
+         */
+        $databaseTool = self::getContainer()->get(DatabaseToolCollection::class)->get();
+        $databaseTool->loadFixtures([UserFixtures::class]);
+
+        $userModel = $this->getUserModel();
+        $userModel->setEmail('test@test.com');
+
+        $errors = $this->validator->validate($userModel);
+        $this->assertEquals(1, $errors->count());
+
+        $mailError = $errors->get(0);
+        $this->assertEquals('email', $mailError->getPropertyPath());
+        $this->assertEquals('Email "test@test.com" is already exists', $mailError->getMessage());
     }
 
     private function getUserModel(): UserModel
